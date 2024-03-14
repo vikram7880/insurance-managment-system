1f1fa99858033066e9f3158941d38fcf24856236
vikram7880 <vikramkund738@gmail.com>
2024-03-14 18:26:44 +0530

    ..

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -4,15 +4,81 @@ import Navbar from './(main)/navbar'
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div role="alert" className="alert alert-info">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        <span>New software update available.</span>
+  {/* Hero */}
+  <div className="bg-slate-900">
+    <div className="bg-gradient-to-b from-violet-600/[.15] via-transparent">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+        {/* Announcement Banner */}
+        <div className="flex justify-center">
+          <a
+            className="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 ps-4 rounded-full shadow-md"
+            href="../figma.html"
+          >
+            <p className="me-2 inline-block text-white text-sm">
+              Preline UI Figma is live.
+            </p>
+            <span className="group-hover:bg-white/[.1] py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-white/[.075] font-semibold text-white text-sm">
+              <svg
+                className="flex-shrink-0 size-4"
+                width={16}
+                height={16}
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+              </svg>
+            </span>
+          </a>
+        </div>
+        {/* End Announcement Banner */}
+        {/* Title */}
+        <div className="max-w-3xl text-center mx-auto">
+          <h1 className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Now it's easier than ever to build products
+          </h1>
+        </div>
+        {/* End Title */}
+        <div className="max-w-3xl text-center mx-auto">
+          <p className="text-lg text-gray-400">
+            Preline is a large open-source project, crafted with Tailwind CSS
+            framework by Hmlstream.
+          </p>
+        </div>
+        {/* Buttons */}
+        <div className="text-center">
+          <a
+            className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800"
+            href="#"
+          >
+            Get started
+            <svg
+              className="flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
+        </div>
+        {/* End Buttons */}
       </div>
-    </>
+    </div>
+  </div>
+  {/* End Hero */}
+</>
+
   )
 }
 
